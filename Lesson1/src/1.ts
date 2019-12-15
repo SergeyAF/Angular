abd542d7b55c78c28ae9c6c7373f4c6a9f92a8d5
Frolov Sergey <faserge@gmail.com>
2019-12-15 19:18:44 +0200

    Bug Fix

--- Lesson1/src/1.ts
+++ Lesson1/src/1.ts
@@ -1,37 +1,25 @@
 //palindrom
 
-let testString: string = 'axbbxa' 
+let testString: string = 'axbbxa'; 
 
 function checkPalindrom(str: string): boolean {
     let revStr = str.split('').reverse().join('');
-    if  (str === revStr) {
-        return true;
-    }
-    return false;    
+    return str === revStr;
 }
-
 console.log(checkPalindrom(testString));
 
 // Fibonacci
 
-let testNumber: number = 3;
+let testNumber: number = 5;
 
 function fiboSeqence(num:number): number[] {
-    let fiboArray: number[] = [];
-    if (num < 2) return fiboArray;
-    fiboArray.push(1);
-    let a = 0;
-    let b = 1;
-    for (let i = 1; i < num; i++) {
-        let c = a + b;
-        if (c < num) {
-            fiboArray.push(c);
-            a = b;
-            b = c;
-        } else {
-            return fiboArray}
+    let fiboArray: number[] = [1, 1];
+    if (num < 2) return [];
+    let i = 2;
+    while (fiboArray[i-2] + fiboArray[i-1] < num) {
+        fiboArray.push(fiboArray[i-2] + fiboArray[i-1]);
+        i++;
     }
     return fiboArray;
 }
-
 console.log(fiboSeqence(testNumber));
